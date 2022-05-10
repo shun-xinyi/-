0fd7357194ab1ffc3755e46584ee1e21539e41ba
wangshaolill <wsl17629240282@163.com>
2022-05-10 23:35:02 +0800

    1.引入elementplus 2.测试vue2写法 3.修改为history模式

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about">
+        <h1>This is an about page</h1>
+        <el-input v-model="input" placeholder="Please input"/>
+        <el-button type="primary" @click="inputAdd">Primary</el-button>
+    </div>
 </template>
+
+<script>
+export default {
+    data(){
+        return{
+            input: 0
+        }
+    },
+    methods:{
+        inputAdd(){
+            this.input++
+        }
+    }
+}
+
+</script>
