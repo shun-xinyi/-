79544d1fbb66b5faf922f042e3827f5413b04251
Shunxinyi <643633229@qq.com>
2022-05-11 11:23:52 +0800

    proxy,env

---
+++ public/config.js
@@ -0,0 +1,5 @@
+const config = {
+    baseURL: "http://localhost:9090",
+    baseSOCKET: "http://localhost:9091"
+};
+exports.config = config;
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,28 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
+const Gold = require("./public/config");
+
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true,
+    devServer: {
+        port: 85,
+        open: true,
+        proxy: {
+            // detail: https://cli.vuejs.org/config/#devserver-proxy
+            [process.env.VUE_APP_BASE_API]: {
+                target: Gold.config.baseURL,
+                changeOrigin: true,
+                pathRewrite: {
+                    ['^' + process.env.VUE_APP_BASE_API]: ''
+                }
+            },
+            '/api': {
+                target: 'https://restapi.amap.com', // 你接口的域名
+                secure: false, // 如果是https接口，需要配置这个参数
+                changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
+                pathRewrite: {
+                    '^/api': ''   //重写接口
+                }
+            }
+        },
+    },
 })
