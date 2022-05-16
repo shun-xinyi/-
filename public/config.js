9e76026e66015414b06c611018b17d88177a1922
Shunxinyi <643633229@qq.com>
2022-05-16 11:07:58 +0800

    配置文件

--- public/config.js
+++ public/config.js
@@ -1,5 +1,7 @@
 const config = {
-    baseURL: "http://sxy.vaiwan.com",
+    // baseURL: "http://sxy.vaiwan.com",
+    // baseSOCKET: "http://dlw.vaiwan.com",
+    baseURL: "http://localhost:9090",
     baseSOCKET: "http://localhost:9091"
 };
 exports.config = config;
